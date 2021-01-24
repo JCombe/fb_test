36e2497cf6d908db86627d41c374916ffdf0cca5
Julian <biscombe.j@gmail.com>
2021-01-24 18:05:42 +0100

    Step 1

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./NavBar.css";
+
+function NavBar() {
+    return (
+        <div className="bg-gray-900 flex justify-evenly h-auto ">
+            <div className="text-white">Home</div>
+            <div className="text-white">Profile</div>
+            <div className="text-white">Messages</div>
+            <div className="text-red-600">Settings</div>
+        </div>
+    )
+}
+
+export default NavBar
+
